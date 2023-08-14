9256f2c7460f0b081fe0343443c0a9cc18fdf026
Kwon Sang <kwonsg514@gmail.com>
2023-08-14 09:13:40 +0900

    Feat: 투표 앱 기능 추가

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+from polls.models import Question, Choice
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    latest_question_list = Question.objects.all().order_by('created_at')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request: HttpRequest, question_id: int) -> HttpResponse:
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def vote(request: HttpRequest, question_id: int) -> HttpResponse:
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        context = {'question': question,
+                   'error_message': "You didn't select a choice."}
+        return render(request, 'polls/detail.html', context)
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return redirect('polls:results', question_id=question_id)
+
+
+def results(request: HttpRequest, question_id: int) -> HttpResponse:
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
